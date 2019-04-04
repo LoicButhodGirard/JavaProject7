497cde5f7f3e0fb57902fc8b369c30ef986192c0
Leona Risiglione <153898@student.aventus.nl>
2019-04-04 11:53:01 +0200

    add GioEnemy

---
+++ src/com/tutorial/main/GioEnemy.java
@@ -0,0 +1,43 @@
+package com.tutorial.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class GioEnemy extends GameObject {
+	
+	private Handler handler;
+	
+	public GioEnemy(float x, float y, ID id, Handler handler) {
+		super(x, y, id);
+		
+		this.handler = handler;
+		
+		velX = 5;
+		velY = 5;
+		
+	}
+	
+	//damage range
+	public Rectangle getBounds() {
+		return new Rectangle((int)x, (int)y, 2, 2);
+	}
+
+	public void tick() {
+		x += velX;
+		y += velY;
+		
+		//bounce range
+		if(y <= 0 || y >= Game.HEIGHT - 42) velY *= -1;
+		if(x <= 0 || x >= Game.WIDTH - 22) velX *= -1;
+		
+		//trail
+		handler.addObject(new Trail(x, y, ID.Trail, Color.red, 2, 2, 0.05f, handler));
+	}
+
+	public void render(Graphics g) {
+		g.setColor(Color.red);
+		g.fillRect((int)x, (int)y, 2, 2);
+	}
+	
+}
